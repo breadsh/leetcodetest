543d1bf4fe8a7746253477e85d019956282cca12
jiangxiaowei <echodll@hotmail.com>
2017-10-24 23:25:21 +0800

    one medium

--- Roundone/medium/Count_Numbers_with_Unique_Digits.py
+++ Roundone/medium/Count_Numbers_with_Unique_Digits.py
@@ -4,3 +4,12 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        choice=9
+        res=1
+        posichoi=1
+        for i in range(min(n,10)):
+            posichoi*=choice
+            res+=posichoi
+            if i>=1:
+                choice-=1
+        return res
